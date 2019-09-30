11aea1fc496883dfa96774bc0d552e70b41cbe5a
Benjamin <benjamin@solca.io>
2019-09-30 16:46:18 -0700

    SM-2 [~] Remove useless tab

--- code/backend/src/index.js
+++ code/backend/src/index.js
@@ -16,7 +16,7 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 });
 
-    app.get('/users', db.getUsers);
+app.get('/users', db.getUsers);
 app.get('/users/:id', db.getUserById);
 app.post('/users', db.createUser);
 app.put('/users/:id', db.updateUser);
