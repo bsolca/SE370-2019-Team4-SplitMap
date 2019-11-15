1a13bb9ea24c10987ac9f76310896b02ee01e191
Andrew <Kuo>
2019-11-14 16:36:23 -0800

    SM-29 [~] Add sanity checks to shelf adding/updating

--- code/backend/src/shelves-queries.js
+++ code/backend/src/shelves-queries.js
@@ -39,11 +39,20 @@ const createShelf = (request, response) => {
         response.status(400).send(`Invalid y coord.`)
         return -1
     }
+    elephantPool.query('SELECT * FROM shelves WHERE parent_map = $1',[parent_map], (error, result) => {
+        for(var i = 0; i < result.rowCount; i++){
+            if(result.rows[i].x == x && result.rows[i].y == y){
+                response.status(400).send(`Location is already occupied.`)
+                return -1
+            }
+        }
+
     elephantPool.query('INSERT INTO shelves (parent_map, x, y) VALUES ($1, $2, $3)', [parent_map, x, y], (error, result) => {
         if (error) {
             throw error
         }
         response.status(201).send(`Shelf added successfully.`)
+    })
     })
     })
 };
@@ -63,13 +72,35 @@ const deleteShelf = (request, response) => {
 // PUT updated data into existing shelf
 const updateShelf = (request,response) => {
     const id = parseInt(request.params.id);
-    const {parent_map,size} = request.body;
-
-    elephantPool.query('UPDATE shelves SET parent_map = $1, size = $2 WHERE id = $3', [parent_map,size,id], (error,result) => {
+    const {parent_map,x,y} = request.body;
+    elephantPool.query('SELECT * FROM maps WHERE id = $1',[parent_map], (error, result) => {
+        if(result.rowCount == 0) //if there is no map with id parent_map
+        {
+            response.status(400).send(`Map does not exist.`)
+            return -1
+        }
+        if(x < 1 || x > result.rows[0].size_width){
+            response.status(400).send(`Invalid x coord.`)
+            return -1
+        }
+        if(y < 1 || y > result.rows[0].size_height){
+            response.status(400).send(`Invalid y coord.`)
+            return -1
+        }
+    elephantPool.query('SELECT * FROM shelves WHERE parent_map = $1',[parent_map], (error, result) => {
+        for(var i = 0; i < result.rowCount; i++){
+            if(result.rows[i].x == x && result.rows[i].y == y){
+                response.status(400).send(`Location is already occupied.`)
+                return -1
+            }
+        }
+    elephantPool.query('UPDATE shelves SET parent_map = $1, x = $2, y = $3 WHERE id = $4', [parent_map,x,y,id], (error,result) => {
         if (error){
             throw error
         }
         response.status(200).send(`Shelf ID# ${id} has been updated`)
+    })
+    })
     })
 };
 
