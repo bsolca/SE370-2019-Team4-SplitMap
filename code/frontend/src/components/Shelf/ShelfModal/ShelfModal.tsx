b0a4db3ae13918ecbe4f4b9954d1cf3482d66964
bsolca <benjamin@solca.io>
2019-11-26 09:32:14 -0800

    [+] Items on shelf

--- code/frontend/src/components/Shelf/ShelfModal/ShelfModal.tsx
+++ code/frontend/src/components/Shelf/ShelfModal/ShelfModal.tsx
@@ -2,14 +2,43 @@ import React, {Dispatch, SetStateAction} from "react";
 import {Modal} from "antd";
 import {ModalProps} from "antd/es/modal";
 
-interface IShelfProps extends ModalProps {
-    toggleOff: Dispatch<SetStateAction<{x:number, y:number, visible:boolean}>>;
-    x:number;
-    y:number;
+enum itemState {
+    inStock,
+    movingIn,
+    movingOut
 }
 
-function ShelfModal(props:IShelfProps) {
-    const onOk = () => {props.toggleOff({x:props.x, y:props.y, visible:false})};
+interface IShelfLocation {
+    id:number,
+    level:number
+}
+
+interface IItems {
+    title: string,
+    description: string,
+    quantity:number
+    state:itemState,
+    level: number,
+    desiredShelf?:IShelfLocation,
+}
+
+const myItems: IItems[] = [
+    {title: "title", description: "desc1", quantity: 11, state:itemState.inStock, level: 2},
+    {title: "qwerty", description: "desc2", quantity: 22, state:itemState.inStock, level: 1},
+    {title: "asdfg", description: "desc3", quantity: 33, state:itemState.inStock, level: 3},
+    {title: "asdfg", description: "desc3", quantity: 12, state:itemState.movingOut, desiredShelf:{id:12, level:2}, level: 3},
+];
+
+interface IShelfProps extends ModalProps {
+    toggleOff: Dispatch<SetStateAction<{ x: number, y: number, visible: boolean }>>;
+    x: number;
+    y: number;
+}
+
+function ShelfModal(props: IShelfProps) {
+    const onOk = () => {
+        props.toggleOff({x: props.x, y: props.y, visible: false})
+    };
     return <Modal
         title={props.title}
         visible={props.visible}
